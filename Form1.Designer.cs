53c5fd3b23424e222d172754b03bd79f3865d048
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-08-22 10:23:25 +0900

    #1 사용자 입력 결과, 결과 출력 화면 구분 사용자 입력과 결과 내용을 단정되게 정리하도록 패널 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
         }
 
