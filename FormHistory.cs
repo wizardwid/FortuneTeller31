f9d48f84d3a724089532bc04ef05687db145eead
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-08-27 10:16:56 +0900

    Revert "#8 내역 불러오기 구현"
    This reverts commit bb3c6cf8f3c076ee98a6c59998026f447754d3e5.

--- FormHistory.cs
+++ FormHistory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,47 +12,13 @@ namespace FortuneTeller31
 {
     public partial class FormHistory : Form
     {
-        List<string> history;
-        Form1 form1;
-        public FormHistory(Form1 form)
+        public FormHistory()
         {
-            this.form1 = form;
             InitializeComponent();
-            UpdateHistory();
         }
 
-        private void LoadHistory()
+        public FormHistory(Form1 form1)
         {
-            try
-            {
-                string filename = "history.csv";
-                history = File.ReadAllLines(filename).ToList();
-            }
-            catch (FileNotFoundException ex)
-            {
-                MessageBox.Show($"파일을 불러올 수 없습니다.\n(ex.Message)", "파일 없음");
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                MessageBox.Show($"파일에 접근권한이 없습니다.\n(ex.Message)", "파일 문제");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"알 수 없는 오류가 발생했습니다.\n(ex.Message)", "알 수 없는 오류",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void btnRefresh_Click(object sender, EventArgs e)
-        {
-            UpdateHistory();
-        }
-
-        private void UpdateHistory()
-        {
-            LoadHistory();
-            lbHistory.Items.Clear();
-            lbHistory.Items.AddRange(history.ToArray());
         }
     }
 }
